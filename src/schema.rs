2c197863bd293aeea10c842544fe8d1239be20ba
Dani <dani.pous@gmail.com>
2020-05-18 11:34:42 +0200

    enabling executor to look_ahead queries in the resolvers + custom scalar Id example

--- src/schema.rs
+++ src/schema.rs
@@ -102,6 +102,34 @@ impl<'a> &'a dyn Droid {
     }
 }
 
+#[derive(Debug)]
+struct Id(String);
+
+use juniper::{Value, ParseScalarResult, ParseScalarValue};
+
+#[juniper::graphql_scalar(description = "Id")]
+impl<S> GraphQLScalar for Id
+where
+    S: ScalarValue
+{
+    // Define how to convert your custom scalar into a primitive type.
+    fn resolve(&self) -> Value {
+        Value::scalar(self.0.to_owned())
+    }
+
+    // Define how to parse a primitive type into your custom scalar.
+    fn from_input_value(v: &InputValue) -> Option<Id> {
+        v.as_string_value()
+        .map(|v| Id(v.to_owned()))
+    }
+
+    // Define how to parse a string value.
+    fn from_str<'a>(value: ScalarToken<'a>) -> ParseScalarResult<'a, S> {
+        <String as ParseScalarValue<S>>::from_str(value)
+    }
+}
+
+
 pub struct Query;
 
 #[juniper::graphql_object(
@@ -113,19 +141,46 @@ pub struct Query;
 /// The root query object of the schema
 impl Query {
     #[graphql(arguments(id(description = "id of the human")))]
-    fn human(database: &Database, id: String) -> Option<&dyn Human> {
+    fn human(executor: &Executor, database: &Database, id: String) -> Option<&dyn Human> {
+
+        use juniper::{LookAheadSelection, LookAheadMethods};
+
+        let info: LookAheadSelection<_> = executor.look_ahead();
+        println!("{:#?}", info);
+
+        let friends = info.children().iter().find(|c| c.field_name() == "friends").unwrap();
+
         database.get_human(&id)
     }
 
+    #[graphql(arguments(id(description = "id of the human as an Id type")))]
+    fn humanById(executor: &Executor, database: &Database, id: Id) -> Option<&dyn Human> {
+        let info = executor.look_ahead();
+        println!("{:#?}", info);
+
+        database.get_human(&id.0)
+    }
+
     #[graphql(arguments(id(description = "id of the droid")))]
-    fn droid(database: &Database, id: String) -> Option<&dyn Droid> {
+    fn droid(executor: &Executor, database: &Database, id: String) -> Option<&dyn Droid> {
+        let info = executor.look_ahead();
+        println!("{:#?}", info);
+
         database.get_droid(&id)
     }
 
     #[graphql(arguments(episode(
         description = "If omitted, returns the hero of the whole saga. If provided, returns the hero of that particular episode"
     )))]
-    fn hero(database: &Database, episode: Option<Episode>) -> Option<&dyn Character> {
+    fn hero(executor: &Executor, database: &Database, episode: Option<Episode>) -> Option<&dyn Character> {
+        let info = executor.look_ahead();
+        println!("{:#?}", info);
+
         Some(database.get_hero(episode).as_character())
+    }
+
+    // Gain access to the executor, which allows you to do look aheads, for example for improved database queries.
+    fn with_executor(_executor: &Executor) -> bool {
+        true
     }
 }
